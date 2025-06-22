cf84e42f1906e0edcad00e08b926e3b5289d5bea
jarvondigital <jarvondigital@gmail.com>
2025-06-22 00:27:59 -0500

    Initial Commit - Update site that has more information - Updated angular to latest version - WIP for design and simplifaction of code

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import {setBasePath} from '@shoelace-style/shoelace';
+
+setBasePath("https://cdn.jsdelivr.net/npm/@shoelace-style/shoelace@2.20.1/cdn/")
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
